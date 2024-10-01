d1acdef5d3692bef78116f41dfbda796659736b6
HieuTT <trtrghieu@gmail.com>
2024-10-01 13:23:39 +0700

    add errorhandler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -228,6 +228,7 @@ def create_app(test_config=None):
                 questions = question_query.all()
             else :
                 questions = []
+                
             if len(questions) == 0: 
                 return jsonify({
                     'question': None,
@@ -246,6 +247,20 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request(error):
+        return (
+            jsonify({"success": False, "error": 400, "message": "bad request."}),
+            400,
+        )
+        
+    @app.errorhandler(500)
+    def bad_request(error):
+        return (
+            jsonify({"success": False, "error": 500, "message": "internal server error"}),
+            500,
+        )
+        
     @app.errorhandler(404)
     def not_found(error):
         return (
